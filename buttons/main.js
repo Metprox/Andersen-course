787915dd724e6cf912d56eb315083afe6d4941c6
Oleg Vakarchuk <olegvakarchuk94@gmail.com>
2017-03-02 15:10:57 +0200

    six buttons in console

---
+++ buttons/main.js
@@ -0,0 +1,30 @@
+let addBtn1 = document.getElementById('mainbtn1');
+  addBtn1.addEventListener('click', () => {
+    let value = document.getElementById('value').value;
+    console.log('Button-1: ', value);   
+  });
+  let addBtn2 = document.getElementById('mainbtn2');
+  addBtn2.addEventListener('click', () => {
+    let value = document.getElementById('value').value;
+    console.log('Button-2: ', value);   
+  });
+  let addBtn3 = document.getElementById('mainbtn3');
+  addBtn3.addEventListener('click', () => {
+    let value = document.getElementById('value').value;
+    console.log('Button-3: ', value);   
+  });
+  let addBtn4 = document.getElementById('mainbtn4');
+  addBtn4.addEventListener('click', () => {
+    let value = document.getElementById('value').value;
+    console.log('Button-4: ', value);   
+  });
+  let addBtn5 = document.getElementById('mainbtn5');
+  addBtn5.addEventListener('click', () => {
+    let value = document.getElementById('value').value;
+    console.log('Button-5: ', value);   
+  });
+  let addBtn6 = document.getElementById('mainbtn6');
+  addBtn6.addEventListener('click', () => {
+    let value = document.getElementById('value').value;
+    console.log('Button-6: ', value);   
+  });
